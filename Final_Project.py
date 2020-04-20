19fb8bec57b3de1f0c5e4195a64972a2d398890e
adriannaperdomo <62344695+adriannaperdomo@users.noreply.github.com>
2020-04-20 16:06:14 -0400

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -1,17 +1,46 @@
 import googlemaps
 from datetime import datetime
 
-gmaps = googlemaps.Client(key='Add Your Key here')
 
-# Geocoding an address
+gmaps = https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
 geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-
-# Look up an address with reverse geocoding
 reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
-# Request directions via public transit
 now = datetime.now()
 directions_result = gmaps.directions("Sydney Town Hall",
                                      "Parramatta, NSW",
                                      mode="transit",
                                      departure_time=now)
+
+
+import (/data/{index}/attributes/wheelchair_boarding)  
+mbta = https://api-v3.mbta.com/docs/swagger/swagger.json
+stop_name = directions_result
+wheelchair acessible = using_wheelchair(directions_result)
+
+
+import urllib.request
+import json
+from pprintimport pprint
+
+
+YOUR_API_KEY = 'gmaps'
+address = 'geocode_result'
+url = f'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY'
+
+
+f = urllib.request.urlopen(url)
+response_text = f.read().decode('utf-8')
+response_data = json.loads(response_text)
+pprint.pprint(response_data)
+
+
+for gmaps in ['geocode_result']:
+    pprint.pprint(response_data['directions_result'])
+    pprint.pprint(using_wheelchair)
+        if using_wheelchair = 0, print('Infomration not Available')
+        if using_wheelchair = 1, print('Wheelchair Acessible')
+        if using_wheelchair = 2, print('No Wheelchair Access')
+    print('The nearest MBTA stop is' stop_name['directions_result'])
+    print('Wheelchair Access:' using_wheelchair['directions_result'])
+                                     
+pip install Flask
